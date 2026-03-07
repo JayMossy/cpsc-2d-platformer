a210f9573c294ea117a829fc784f53f5f2aba591
Anthony Baca <anthonybaca@my.cwi.edu>
2026-03-07 13:46:27 -0700

    cleaned up the comments

--- src/js/systems/physics.js
+++ src/js/systems/physics.js
@@ -8,29 +8,11 @@ export function applyGravity(player, dt) {
     if (player.vy > 0) {
         // falling faster
         player.vy += GRAVITY * 1.5 * dt;
-        // console.log(player.vy, "fall");
     } else {
         // normal gravity while rising
         player.vy += GRAVITY * dt;
-        // console.log(player.vy, "jump");
     }
 }
-
-
-/*
-// Both of these function are always being called, and im guessing you only wanted applyFallSpeedUp when jumping
-export function applyGravity(player, dt) {
-    player.vy += GRAVITY * dt;
-    console.log("app")
-}
-
-export function applyFallSpeedUp(player, dt) {
-    if (player.vy > 0) {
-            player.vy += GRAVITY * 0.5 * dt;
-        }
-    console.log("fall")
-}
-*/
 
 export function clampFallSpeed(player) {
     if (player.vy > player.maxFallSpeed) {
@@ -46,13 +28,6 @@ export function clampFallSpeed(player) {
 export function setMovementX(player, direction) {
     player.vx = direction * player.moveSpeed;
 }
-
-/*
-// Don't need function just for stopping x movement 
-export function stopMovingX(player) {
-    player.vx = 0;
-}
-*/
 
 export function integrate(player, dt) {
     player.x += player.vx * dt;
