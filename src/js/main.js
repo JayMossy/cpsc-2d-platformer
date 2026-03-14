7c62a35c50a62653912b5d4064c7b1de7cb29c7b
Evan Varozza <163042394+VarozzaEJ@users.noreply.github.com>
2026-03-13 23:43:28 -0600

    Saving score to storage (#26)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import { horizontal, vertical } from "./systems/mapCollision.js";
 import { playerMovement } from "./systems/playerMovement.js";
 import { render } from "./systems/render.js";
 import { coinAnimator } from "./systems/coins.js";
@@ -9,6 +10,8 @@ let lastTime = 0;
 export const enemies = [];
 enemies.push(new Enemy(240, 1200))
 
+
+
 function loop(timestamp) {
 
     if (lastTime === 0) lastTime = timestamp;
@@ -18,10 +21,10 @@ function loop(timestamp) {
 
     if (dt > 0.1) dt = 0.1;
 
-    playerMovement(dt);  
+    playerMovement(dt);
     for (const enemy of enemies) {
         enemy.update(dt, player);
-    } 
+    }
     coinAnimator.update(dt); // Should make a file in the future that will hold all collectibles to not clutter up main.js.
     render();
 
