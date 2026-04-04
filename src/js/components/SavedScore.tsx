6d5f904cdd787fbdfa17e6a77954842f8891fe7e
Anthony Baca <anthonybaca@my.cwi.edu>
2026-04-04 00:06:37 -0600

    fixing main menu styles

--- src/js/components/SavedScore.tsx
+++ src/js/components/SavedScore.tsx
@@ -22,21 +22,25 @@ const SavedScore = () => {
       window.removeEventListener("scoresUpdated", handleScoresUpdate);
   }, []);
     return (
-        <>
-            <div className="container score-board">
+    <>
+      <div className="container score-board">
         <div className="">
+
           <div className="row">
             <h2 className="score-title text-center">High Scores</h2>
           </div>
+
           {scores.length > 0 ? (
             scores.map((score: Score, index: number) => (
               <div
                 key={score.name}
                 className="score-item row mb-3  p-3 border rounded"
               >
+                  
                 <div className="col-12 score-name text-warning fw-bold">
                   #{index + 1} {score.name}
                 </div>
+
                 <div className="row mt-2">
                   <div className="col-12 col-md-6 score-label">
                     Highest Level Reached:
@@ -45,12 +49,14 @@ const SavedScore = () => {
                     {score.highestLevelAchieved}
                   </div>
                 </div>
+
                 <div className="row">
                   <div className="col-md-6 col-12 score-label">High Score:</div>
                   <div className="col-md-6 col-12 score-value text-primary fw-bold">
                     {score.highScore}
                   </div>
                 </div>
+                
               </div>
             ))
           ) : (
