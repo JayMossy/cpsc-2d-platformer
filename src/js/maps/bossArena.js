6b0a38273a770749715b752e46be7dfe329e5a9f
AnthonyBaca <anthonybaca@my.cwi.edu>
2026-04-20 12:21:49 -0600

    Improve arena (#63)

--- src/js/maps/bossArena.js
+++ src/js/maps/bossArena.js
@@ -15,15 +15,17 @@ export const TILES = {
   COLUMN_BOTTOM_RIGHT: 8,
   COLUMN_MIDDLE_LEFT: 9,
   COLUMN_MIDDLE_RIGHT: 10,
-  BACKGROUND: 11,
-  FLOOR_CORNER_LEFT: 12,
-  FLOOR_LEFT: 13,
-  FLOOR_CORNER_RIGHT: 14,
-  FLOOR_RIGHT : 15,
-  CEILING: 16,
-  CEILING_RIGHT: 17,
-  CEILING_LEFT: 18,
-  CHAIR: 19, 
+  COLUMN_TOP_LEFT: 11,
+  COLUMN_TOP_RIGHT: 12,
+  BACKGROUND: 13,
+  FLOOR_CORNER_LEFT: 14,
+  WALL_LEFT: 15,
+  FLOOR_CORNER_RIGHT: 16,
+  WALL_RIGHT : 17,
+  CEILING: 18,
+  CEILING_RIGHT: 19,
+  CEILING_LEFT: 20,
+  CHAIR: 21, 
   TREE1: 24,
   TREE2: 25,
   SPIKE: -1
@@ -44,34 +46,27 @@ export const tileLocation = {
   colBtnR: [16*6, 16*11],
   colMidL: [16*5, 16*10],
   colMidR: [16*6, 16*10],
+  colTopL: [16*5, 16*12],
+  colTopR: [16*6, 16*12],
   background: [16*6, 16*15],
   floorCornerL: [16*4, 16*2],
-  floorLeft: [16*4, 16*3],
+  wallLeft: [16*4, 16*3],
   floorCornerR: [16*0, 16*2],
-  floorRight: [16*0, 16*3],
+  wallRight: [16*0, 16*3],
   ceil: [16*1, 16*5],
   ceilR: [16*0, 16*5],
   ceilL: [16*4, 16*5],
   tree1: [16*13, 16*5.5],
   tree2: [16*10.5, 16*48],
-  chair: [700, 300]
+  chair: [500, 200]
 };
-
-const now = new Date();
-const rndNumber = 
-      now.getHours() * 439 + 
-      now.getMinutes() * 577 + 
-      now.getSeconds() * 727;
 
 export const map = Array.from({ length: Mrows }, () =>
   Array.from({ length: Mcols }, () => TILES.SKY)
 );
 
-// Outside square
-// makePlatform(1, Mrows-30, 0, Mcols, TILES.BLACK, TILES.DIRT);
 
 /* --- Beginning --- */
-
 // Top dirt
 makeSquare(1, Mrows-25, 0, Mcols, TILES.DARK, TILES.DIRT)
 
@@ -86,7 +81,7 @@ makeSquare(Mrows-20, Mrows-20, 30, 30, TILES.TREE1);
 makeSquare(Mrows-20, Mrows-20, 35, 35, TILES.TREE1);
 makeSquare(Mrows-20, Mrows-20, 40, 41, TILES.TREE2);
 makeSquare(Mrows-20, Mrows-20, 50, 50, TILES.TREE2);
-makeSquare(Mrows-20, Mrows-20, 55, 55, TILES.CHAIR);
+makeSquare(Mrows-20, Mrows-20, 55, 55, TILES.TREE1);
 
 // Mixing in tiles with dirt - to paved floor
 mixTiles(25, [TILES.DIRT, TILES.BROWN_BRICK1], 50, 65);
@@ -102,53 +97,50 @@ mixTiles(25, [TILES.BRICK1, TILES.PAVED_FLOOR], 115, 125);
 // Room
 makeSquare(Mrows-24, Mrows, 150, Mcols, TILES.BACKGROUND);
 // Columns
-makeColumn(160, 24, 20);
-makeColumn(170, 24, 20);
-makeColumn(180, 24, 20);
-makeColumn(190, 24, 20);
-makeColumn(200, 24, 20);
-makeColumn(210, 24, 20);
+makeColumn(160, 24, 8);
+makeColumn(170, 24, 8);
+makeColumn(180, 24, 8);
+makeColumn(190, 24, 8);
+makeColumn(200, 24, 8);
+makeColumn(210, 24, 8);
 
 // Ceil
 makeSquare(Mrows-15, Mrows, 150, Mcols, TILES.DARK);
 makeSquare(Mrows-15, Mrows-15, 150, Mcols, TILES.CEILING)
-makeSquare(Mrows-15, Mrows, 150, 150, TILES.FLOOR_RIGHT)
+makeSquare(Mrows-15, Mrows, 150, 150, TILES.WALL_RIGHT)
 makeSquare(Mrows-15, Mrows-15, 150, 150, TILES.CEILING_RIGHT)
-makeSquare(Mrows-15, Mrows, 214, 214, TILES.FLOOR_LEFT)
+makeSquare(Mrows-15, Mrows, 214, 214, TILES.WALL_LEFT)
 makeSquare(Mrows-15, Mrows-15, 214, 214, TILES.CEILING_LEFT)
 
-makeSquare(Mrows-15, Mrows, 281, 281, TILES.FLOOR_RIGHT)
+makeSquare(Mrows-15, Mrows, 281, 281, TILES.WALL_RIGHT)
 makeSquare(Mrows-15, Mrows-15, 281, 281, TILES.CEILING_RIGHT)
 
 
 // Hole
 makeSquare(5, Mrows, 215, 280, TILES.BACKGROUND);
-makeSquare(15, Mrows-26, 214, 214, TILES.FLOOR_LEFT);
+makeSquare(15, Mrows-26, 214, 214, TILES.WALL_LEFT);
 makeSquare(Mrows-25, Mrows-25, 214, 214, TILES.FLOOR_CORNER_LEFT);
-makeSquare(6, Mrows-26, 280, 280, TILES.FLOOR_RIGHT);
+makeSquare(6, Mrows-26, 280, 280, TILES.WALL_RIGHT);
 makeSquare(5, 5, 280, 280, TILES.DARK)
 makeSquare(Mrows-25, Mrows-25, 280, 280, TILES.FLOOR_CORNER_RIGHT);
 
 
 /* --- Inside --- */
-// carving out inside square
+// Carving out inside square
 makeSquare(5, 30, 50, 214, TILES.BACKGROUND, TILES.CEILING);
+makeSquare(30, 30, 214, 214, TILES.CEILING_LEFT)
 makeSquare(5, 60, 4, 50, TILES.BACKGROUND, TILES.CEILING);
+makeSquare(5, 60, 4, 4, TILES.WALL_LEFT)
+makeSquare(5, 5, 4, 4, TILES.DARK)
 // Columns
-makeColumn(190, Mrows-5, 24)
-makeColumn(185, Mrows-5, 24)
-makeColumn(180, Mrows-5, 24)
-makeColumn(175, Mrows-5, 24)
-makeColumn(170, Mrows-5, 24)
-makeColumn(165, Mrows-5, 24)
+makeColumn(190, Mrows-6, 23)
+makeColumn(180, Mrows-6, 23)
+makeColumn(170, Mrows-6, 23)
+makeColumn(160, Mrows-6, 23)
+makeColumn(150, Mrows-6, 23)
+makeColumn(140, Mrows-6, 23)
 // Top floor
-makeSquare(5, 5, 0, 279, TILES.PAVED_FLOOR)
-
-// Chair
-makeSquare(10, 10, 15, 15, TILES.CHAIR);
-
-
-
+makeSquare(5, 5, 5, 279, TILES.PAVED_FLOOR)
 
 function mixTiles(y, tilesArr, start, stop) {
   for(let x = start; x < stop; x++){
@@ -166,9 +158,12 @@ function makeColumn(x, y, height) {
     map[y][x] = TILES.COLUMN_MIDDLE_LEFT;
     map[y][x+1] = TILES.COLUMN_MIDDLE_RIGHT;
   }
+  map[y][x] = TILES.COLUMN_TOP_LEFT;
+  map[y][x+1] = TILES.COLUMN_TOP_RIGHT;
 }
 
 /**
+* Makes squares, you can set the top tile as a different one
 * Makes squares, you can set the top tile as a different one
 * @param {*} bottom - Bottom start position
 * @param {*} top - Top end position
--- src/js/maps/bossArenaRender.js
+++ src/js/maps/bossArenaRender.js
@@ -15,14 +15,13 @@ export class BossArena extends BaseRender {
             "/assets/sprites/tiles/boss-tiles-2.png",
         );
 
-        this.now = new Date();
-        this.rndNumber = this.now.getHours() * 439 + this.now.getMinutes() * 577 + this.now.getSeconds() * 727;
-
         this.background = new Image();
         this.background.src = "/assets/backgrounds/bg-boss-4.png";
 
         this.throne = new Image();
-        this.throne.src = "/assets/backgrounds/throan.png";
+        this.throne.src = "/assets/sprites/tiles/throne.png";
+
+        this.tileData = this.createTiles();
     }
 
     drawMap() {
@@ -36,36 +35,11 @@ export class BossArena extends BaseRender {
             this.ctx.drawImage(this.background, Math.floor(x), 0, drawWidth, this.canvas.height);
         }
 
-        const ogSize = tileLocation.tileSize;
-        // SourceX   ,   SourceY
-        const [darkSourceX, darkSourceY] = tileLocation.dark;
-        const [dirtSourceX, dirtSourceY] = tileLocation.dirt;
-        const [pFloorSourceX, pFloorSourceY] = tileLocation.pavedFloor;
-        const [brick1SourceX, brick1SourceY] = tileLocation.brick1;
-        const [brick2SourceX, brick2SourceY] = tileLocation.brick2;
-        const [bBrick1SourceX, bBrick1SourceY] = tileLocation.brownBrick1;
-        const [bBrick2SourceX, bBrick2SourceY] = tileLocation.brownBrick2;
-        const [colBtnLSourseX, colBtnLSourseY] = tileLocation.colBtnL;
-        const [colBtnRSourseX, colBtnRSourseY] = tileLocation.colBtnR;
-        const [colMidLSourseX, colMidLSourseY] = tileLocation.colMidL;
-        const [colMidRSourseX, colMidRSourseY]  = tileLocation.colMidR;
-        const [bgSourseX, bgSourseY] = tileLocation.background;
-        const [floorLSourseX, floorLSourseY] = tileLocation.floorLeft;
-        const [floorCrnLSourseX, floorCrnLSourseY] = tileLocation.floorCornerL;
-        const [ceilSourceX, ceilSourceY] = tileLocation.ceil
-        const [floorRSourseX, floorRSourseY] = tileLocation.floorRight;
-        const [floorCrnRSourseX, floorCrnRSourseY] = tileLocation.floorCornerR;
-        const [ceilRSourceX, ceilRSourceY] = tileLocation.ceilR;
-        const [ceilLSourceX, ceilLSourceY] = tileLocation.ceilL;
-        const [tree1SourceX, tree1SourceY] = tileLocation.tree1;
-        const [tree2SourceX, tree2SourceY] = tileLocation.tree2;
-        const [chiarSourceX, chiarSourceY] = tileLocation.chair;
+        const startCol = Math.max(0, Math.floor(this.camera.x / tileSize)-2);
+        const endCol = Math.min(Mcols, Math.ceil((this.camera.x + this.canvas.width) / tileSize)+2);
 
-        const startCol = Math.max(0, Math.floor(this.camera.x / tileSize));
-        const endCol = Math.min(Mcols, Math.ceil((this.camera.x + this.canvas.width) / tileSize));
-
-        const startRow = Math.max(0, Math.floor(this.camera.y / tileSize));
-        const endRow = Math.min(Mrows, Math.ceil((this.camera.y + this.canvas.height) / tileSize));
+        const startRow = Math.max(0, Math.floor(this.camera.y / tileSize)-2);
+        const endRow = Math.min(Mrows, Math.ceil((this.camera.y + this.canvas.height) / tileSize)+2);
 
         for (let y = startRow; y < endRow; y++) {
             for (let x = startCol; x < endCol; x++) {
@@ -81,121 +55,42 @@ export class BossArena extends BaseRender {
 
                 const tile = this.map[y][x];
 
-                this.ctx.fillStyle = "rgba(0,0,0,0)";
-                this.ctx.fillRect(tileX, tileY, this.tileSize, this.tileSize);
+                const tileInfo = this.tileData[tile];
 
-                if (tile === TILES.DARK) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, darkSourceX, darkSourceY, ogSize, ogSize, tileX, tileY, this.tileSize, this.tileSize);
-                }
+                if (!tileInfo) continue;
 
-                if (tile === TILES.DIRT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, dirtSourceX, dirtSourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-                
-                if (tile === TILES.PAVED_FLOOR) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, pFloorSourceX, pFloorSourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
+                const [srcX, srcY] = tileInfo.srcLoc;
+                const imgSrc = tileInfo.imgSrc;
 
-                if (tile === TILES.BRICK1) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, brick1SourceX, brick1SourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
+                if (!imgSrc.complete || imgSrc.naturalWidth === 0) continue;
 
-                if (tile === TILES.BRICK2) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, brick2SourceX, brick2SourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-                
-                if (tile === TILES.BROWN_BRICK1) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, bBrick1SourceX, bBrick1SourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.BROWN_BRICK2) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, bBrick2SourceX, bBrick2SourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.COLUMN_BOTTOM_LEFT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, colBtnLSourseX, colBtnLSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.COLUMN_BOTTOM_RIGHT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, colBtnRSourseX, colBtnRSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.COLUMN_MIDDLE_LEFT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, colMidLSourseX, colMidLSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.COLUMN_MIDDLE_RIGHT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, colMidRSourseX, colMidRSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.BACKGROUND) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, bgSourseX, bgSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.FLOOR_CORNER_LEFT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, floorCrnLSourseX, floorCrnLSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.FLOOR_LEFT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, floorLSourseX, floorLSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.FLOOR_CORNER_RIGHT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, floorCrnRSourseX, floorCrnRSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.FLOOR_RIGHT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, floorRSourseX, floorRSourseY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.CEILING) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, ceilSourceX, ceilSourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.CEILING_RIGHT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, ceilRSourceX, ceilRSourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.CEILING_LEFT) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, ceilLSourceX, ceilLSourceY, ogSize, ogSize, tileX, tileY, tileSize, tileSize);
-                }
-
-                if (tile === TILES.TREE1) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, tree1SourceX, tree1SourceY, 16*4, 16*4.5, tileX, tileY, 16*10, 16*10);
-                }
-
-                if (tile === TILES.TREE2) {
-                    if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                    this.ctx.drawImage(this.tileSet, tree2SourceX, tree2SourceY, 16*4, 16*4, tileX, tileY, 16*10, 16*10);
-                }
-
-                // if (tile === TILES.CHAIR) {
-                //     if (!this.tileSet.complete || this.tileSet.naturalWidth === 0) continue;
-                //     this.ctx.drawImage(this.throne, chiarSourceX, chiarSourceY, 500, 500, tileX, tileY, 16*20, 16*20);
-                // }
+                this.ctx.drawImage(
+                    imgSrc,
+                    srcX,
+                    srcY,
+                    tileInfo.w,
+                    tileInfo.h,
+                    tileX,
+                    tileY,
+                    tileInfo.drawW ?? tileSize,
+                    tileInfo.drawH ?? tileSize
+                );
 
             }
         }
+
+        // Throne 
+        this.ctx.drawImage(
+            this.throne,
+            500,
+            200,
+            550,
+            750,
+            25 * this.tileSize - this.camera.x,
+            (Mrows-16) * this.tileSize - this.camera.y,
+            16*40,
+            16*40
+        );
     }
 
     render() {
@@ -203,4 +98,160 @@ export class BossArena extends BaseRender {
         //TODO: Add boss
     }
 
+    createTiles(){
+        const og = tileLocation.tileSize;
+
+        return {
+            [TILES.DARK]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.dark,
+                w: og,
+                h: og
+            },
+            [TILES.DIRT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.dirt,
+                w: og,
+                h: og
+            },
+            [TILES.PAVED_FLOOR]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.pavedFloor,
+                w: og,
+                h: og
+            },
+            [TILES.BRICK1]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.brick1,
+                w: og,
+                h: og
+            },
+            [TILES.BRICK2]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.brick2,
+                w: og,
+                h: og
+            },
+            [TILES.BROWN_BRICK1]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.brownBrick1,
+                w: og,
+                h: og
+            },
+            [TILES.BROWN_BRICK2]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.brownBrick2,
+                w: og,
+                h: og
+            },
+            [TILES.COLUMN_BOTTOM_LEFT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.colBtnL,
+                w: og,
+                h: og
+            },
+            [TILES.COLUMN_BOTTOM_RIGHT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.colBtnR,
+                w: og,
+                h: og
+            },
+            [TILES.COLUMN_MIDDLE_LEFT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.colMidL,
+                w: og,
+                h: og
+            },
+            [TILES.COLUMN_MIDDLE_RIGHT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.colMidR,
+                w: og,
+                h: og
+            },
+            [TILES.COLUMN_TOP_LEFT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.colTopL,
+                w: og,
+                h: og
+            },
+            [TILES.COLUMN_TOP_RIGHT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.colTopR,
+                w: og,
+                h: og
+            },
+            [TILES.BACKGROUND]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.background,
+                w: og,
+                h: og
+            },
+            [TILES.FLOOR_CORNER_LEFT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.floorCornerL,
+                w: og,
+                h: og
+            },
+            [TILES.FLOOR_CORNER_RIGHT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.floorCornerR,
+                w: og,
+                h: og
+            },
+            [TILES.CEILING]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.ceil,
+                w: og,
+                h: og
+            },
+            [TILES.CEILING_LEFT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.ceilL,
+                w: og,
+                h: og
+            },
+            [TILES.CEILING_RIGHT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.ceilR,
+                w: og,
+                h: og
+            },
+            [TILES.WALL_LEFT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.wallLeft,
+                w: og,
+                h: og
+            },
+            [TILES.WALL_RIGHT]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.wallRight,
+                w: og,
+                h: og
+            },
+            [TILES.TREE1]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.tree1,
+                w: 16*4,
+                h: 16*4.5,
+                drawW: 16*10,
+                drawH: 16*10
+            },
+            [TILES.TREE2]: {
+                imgSrc: this.tileSet,
+                srcLoc: tileLocation.tree2,
+                w: 16*4,
+                h: 16*4,
+                drawW: 16*10,
+                drawH: 16*10
+            },
+            [TILES.CHAIR]: {
+                imgSrc: this.throne,
+                srcLoc: tileLocation.chair,
+                w: 550,
+                h: 750,
+                drawW: 128,
+                drawH: 128
+            }
+        };
+    }
 }
