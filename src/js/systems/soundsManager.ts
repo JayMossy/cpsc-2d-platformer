f96b80f79a2601083640c92fa790256169e9aac6
Anthony Baca <anthonybaca@my.cwi.edu>
2026-04-22 19:42:17 -0600

    music stays even after refershing page

--- src/js/systems/soundsManager.ts
+++ src/js/systems/soundsManager.ts
@@ -1,14 +1,14 @@
 const sounds: Record<string, HTMLAudioElement> = {
-    jump: new Audio("../../assets/audio/sfx/jump.wav"),
-    powerUp: new Audio("../../assets/audio/sfx/power_up.wav"),
-    coin: new Audio("../../assets/audio/sfx/coin.wav")
+  jump: new Audio("../../assets/audio/sfx/jump.wav"),
+  powerUp: new Audio("../../assets/audio/sfx/power_up.wav"),
+  coin: new Audio("../../assets/audio/sfx/coin.wav")
 };
 
 function playSound(name: string): void {
-    console.log("Played:", name);
-    const sound = sounds[name];
-    sound.currentTime = 0;
-    sound.play();
+  console.log("Played:", name);
+  const sound = sounds[name];
+  sound.currentTime = 0;
+  sound.play();
 }
 
 const bgMusic: HTMLAudioElement = new Audio("../../assets/audio/music/bgMusicTemp.mp3");
@@ -16,8 +16,15 @@ bgMusic.loop = true;
 bgMusic.volume = 0.5;
 
 export function startBGMusic(): void {
-  bgMusic.play();
+  bgMusic.play().catch(() => { });
+  window.removeEventListener("click", startBGMusic);
+  window.removeEventListener("keydown", startBGMusic);
 }
+
+window.addEventListener("load", () => {
+  window.addEventListener("click", startBGMusic);
+  window.addEventListener("keydown", startBGMusic);
+});
 
 export function stopBGMusic(): void {
   bgMusic.pause();
